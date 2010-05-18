f611d179fdbe9548c83fe3e868c2e90d771fa9f4
pfefferle <pfefferle@46d9b22f-1b99-97f8-f5b3-7c039774b3dc>
2010-05-18 07:52:17 +0000

    moved to trunk

---
+++ contentscript.js
@@ -0,0 +1,21 @@
+findActivityStreamLinks();
+
+// See if the document contains a <link> tag within the <head> and
+// whether that points to an RSS feed.
+function findActivityStreamLinks() {
+  // Find all the activity stream link elements.
+  var result = document.evaluate('//*[local-name()="link"][contains(@rel, "activitystream") or contains(@class, "activitystream")]', document, null, 0, null);
+
+  var feeds = [];
+  var item;
+  var count = 0;
+  while (item = result.iterateNext()) {
+    feeds.push({"href": item.href, "title": item.title});
+    ++count;
+  }
+
+  if (count > 0) {
+    // Notify the extension needs to show the activity streams action icon.
+    chrome.extension.sendRequest({msg: "activityIcon", feeds: feeds});
+  }
+}
